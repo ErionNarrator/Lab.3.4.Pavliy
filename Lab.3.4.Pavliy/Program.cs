ee016e1d1f416f5a268716f07bc456051a5c95cc
Nikita Pavliy <nikitapavliy@gmail.com>
2023-12-25 14:23:01 +0200

    Добавьте файлы проекта.

---
+++ Lab.3.4.Pavliy/Program.cs
@@ -0,0 +1,31 @@
+﻿try
+{
+    Console.WriteLine("Введите количество выполнений цикла:");
+    int n = int.Parse(Console.ReadLine());
+    Console.WriteLine("Введите x:");
+    double x = double.Parse(Console.ReadLine());
+    double s = 1;
+
+    for (int i = 1; i <= n; i++)
+    {
+        long f = 1;
+
+        for (int j = 1; j <= 2 * i + 1; j += 2)
+            f *= j;
+
+        if (i % 2 == 0)
+        {
+            s += Math.Pow(-2 * x, 2 * i + 1) / f;
+        }
+        else
+        {
+            s -= Math.Pow(-2 * x, 2 * i + 1) / f;
+        }
+    }
+
+    Console.WriteLine($"S = {s:F2}");
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
